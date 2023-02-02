1f63e749ccdd02f81b431b478896d4b7fbcdfbc8
Fernando Ayasta <fernandoayasta25@gmail.com>
2023-02-02 15:47:43 -0500

    new regex

--- src/test/java/helpers/JSValidators/DateValidatorFormat2.js
+++ src/test/java/helpers/JSValidators/DateValidatorFormat2.js
@@ -8,7 +8,7 @@ function fn(dateString) {
     //     karate.log('***** invalid date string', s);
     //     return false;
     // }
-    var dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\:\d{3}[+-]\d{2}:\d{2}$/;
+    var dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:\d{2}$/;
     if (dateFormat.test(dateString)) {
         return true;
       } else {
