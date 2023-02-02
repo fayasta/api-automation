9f04b0c77334a0c8dff4442e1797e5a62678feba
Fernando Ayasta <fernandoayasta25@gmail.com>
2023-02-02 14:03:43 -0500

    try regex

--- src/test/java/helpers/JSValidators/DateValidatorFormat2.js
+++ src/test/java/helpers/JSValidators/DateValidatorFormat2.js
@@ -1,11 +1,17 @@
-function fn(s) {
-    var SimpleDateFormat = Java.type("java.text.SimpleDateFormat");
-    var sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
-    try{
-        sdf.parse(s).time;
-        return true;
-    }catch(e){
-        karate.log('***** invalid date string', s);
-        return false;
-    }
+function fn(dateString) {
+    // var SimpleDateFormat = Java.type("java.text.SimpleDateFormat");
+    // var sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+    // try{
+    //     sdf.parse(s).time;
+    //     return true;
+    // }catch(e){
+    //     karate.log('***** invalid date string', s);
+    //     return false;
+    // }
+    var dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}$/;
+    if (dateFormat.test(dateString)) {
+        console.log("Valid date format");
+      } else {
+        console.log("Invalid date format");
+      } 
 }
