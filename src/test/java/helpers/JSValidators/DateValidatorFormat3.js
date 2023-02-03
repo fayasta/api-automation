d8d65a92540b9ddfcc9c6a3ea516de7807592b25
Fernando Ayasta <fernandoayasta25@gmail.com>
2023-02-03 15:14:52 -0500

    fix date comparison

--- src/test/java/helpers/JSValidators/DateValidatorFormat3.js
+++ src/test/java/helpers/JSValidators/DateValidatorFormat3.js
@@ -1,8 +1,23 @@
-function fn(dateString) {
-    var dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}/;
-    if (dateFormat.test(dateString)) {
-        return true;
-      } else {
-        return false;
-      } 
+// function fn(dateString) {
+//     var sub_date_string = dateString.replace('-05:00','');
+//     var dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}/;
+//     if (dateFormat.test(dateString)) {
+//         return true;
+//       } else {
+//         return false;
+//       } 
+// }
+
+function fn(s) {
+  var dateFormat = /\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])/;
+  var subDate = s.match(dateFormat)[0];
+  var SimpleDateFormat = Java.type("java.text.SimpleDateFormat");
+  var sdf = new SimpleDateFormat("yyyy-MM-dd")
+  try{
+      sdf.parse(subDate).time;
+      return true;
+  }catch(e){
+      karate.log('***** invalid date string', s);
+      return false;
+  }
 }
